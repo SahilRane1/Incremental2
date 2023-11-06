a9d9a72e31fcb10cc668d22811af736e00d02ee6
Iamneo <techadmin@iamneo.ai>
2023-11-06 08:21:16 +0000

    Backup Commit

---
+++ dotnetapp/Controllers/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dotnetapp.Models;
+
+namespace dotnetapp.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public PlayerController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+        public IActionResult Index(){
+            
+            return View();
+
+        }
+        public IActionResult Find(int id){
+            var data=context.Players.Find(id);
+            return View(data);
+        }
+        public IActionResult Add(Player p){
+            if(ModelState.IsValid){
+                Player pl=new Player();
+                pl.Name=p.Name;
+                pl.Category=p.Category;
+                pl.BiddingAmount=p.BiddingAmount;
+                context.Players.Add(pl);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(Player p,int id){
+            if(ModelState.IsValid){
+                Player pl=context.Players.Find(id);
+                pl.Name=p.Name;
+                pl.Category=p.Category;
+                pl.BiddingAmount=p.BiddingAmount;
+                context.Players.Add(pl);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int id){
+            var data=context.Players.Find(id);
+            context.Players.Remove(data);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ nunit/run.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+if [ ! -d "/home/coder/project/workspace/dotnetapp/" ]
+then
+    cp -r /home/coder/project/workspace/nunit/dotnetapp /home/coder/project/workspace/;
+fi
+
+if [ -d "/home/coder/project/workspace/dotnetapp/" ]
+then
+    echo "project folder present"
+    # checking for src folder
+    if [ -d "/home/coder/project/workspace/dotnetapp/" ]
+    then
+        cp -r /home/coder/project/workspace/nunit/test/TestProject /home/coder/project/workspace/;
+        cp -r /home/coder/project/workspace/nunit/test/dotnetapp.sln /home/coder/project/workspace/dotnetapp/;
+        cd /home/coder/project/workspace/dotnetapp || exit;
+        dotnet clean;
+        dotnet test;
+    else
+        echo "Week2_day5_Player_ClassExists FAILED";
+        echo "Week2_Day5_Player_Properties_Id_ReturnExpectedDataTypes_int FAILED";
+        echo "Week2_Day5_Player_Properties_BiddingAmount_ReturnExpectedDataTypes_decimal FAILED";
+        echo "Week2_Day6_IndexMethod_PlayerController_Exists FAILED";
+        echo "Week2_Day6_IndexMethod_PlayerController_DisplaysAllPlayers FAILED";
+        echo "Week3_Day2_Test_IndexViewFile_Exists FAILED";
+        echo "Week3_Day2_VerifyIndexRoute FAILED";
+        echo "Week3_Day2_VerifyCreateRoute FAILED";
+        echo "Week3_Day2_Player_NameIsRequired FAILED";
+        echo "Week3_Day2_Player_BiddingAmountMustBeGreaterThanZero FAILED";
+        echo "Week3_Day3_Test_LoginViewFile_Exists FAILED";
+        echo "Week3_Day3_Test_RegisterViewFile_Exists FAILED";
+        echo "Week3_Day3_CreateMethod_PlayerController_Add_Player_to_DB FAILED";
+        echo "Week3_Day5_EditMethod_PlayerController_Exists FAILED";
+        echo "Week3_Day5_DeleteMethod_PlayerController_Exists FAILED";
+        echo "Week3_Day5_Test_CreateViewFile_Exists FAILED";
+        echo "Week3_Day5_Test_EditViewFile_Exists FAILED";
+        echo "Week3_Day5_DeleteMethod_PlayerController_Delete_Player_from_DB FAILED";
+    fi
+else
+    echo "Week2_day5_Player_ClassExists FAILED";
+    echo "Week2_Day5_Player_Properties_Id_ReturnExpectedDataTypes_int FAILED";
+    echo "Week2_Day5_Player_Properties_BiddingAmount_ReturnExpectedDataTypes_decimal FAILED";
+    echo "Week2_Day6_IndexMethod_PlayerController_Exists FAILED";
+    echo "Week2_Day6_IndexMethod_PlayerController_DisplaysAllPlayers FAILED";
+    echo "Week3_Day2_Test_IndexViewFile_Exists FAILED";
+    echo "Week3_Day2_VerifyIndexRoute FAILED";
+    echo "Week3_Day2_VerifyCreateRoute FAILED";
+    echo "Week3_Day2_Player_NameIsRequired FAILED";
+    echo "Week3_Day2_Player_BiddingAmountMustBeGreaterThanZero FAILED";
+    echo "Week3_Day3_Test_LoginViewFile_Exists FAILED";
+    echo "Week3_Day3_Test_RegisterViewFile_Exists FAILED";
+    echo "Week3_Day3_CreateMethod_PlayerController_Add_Player_to_DB FAILED";
+    echo "Week3_Day5_EditMethod_PlayerController_Exists FAILED";
+    echo "Week3_Day5_DeleteMethod_PlayerController_Exists FAILED";
+    echo "Week3_Day5_Test_CreateViewFile_Exists FAILED";
+    echo "Week3_Day5_Test_EditViewFile_Exists FAILED";
+    echo "Week3_Day5_DeleteMethod_PlayerController_Delete_Player_from_DB FAILED";
+fi
+    
