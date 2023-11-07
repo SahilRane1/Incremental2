4150bd614fd9959e3e43a3180044ce9f3d6b0ca5
Iamneo <techadmin@iamneo.ai>
2023-11-07 11:47:23 +0000

    Backup Commit

--- dotnetapp/Models/Player.cs
+++ dotnetapp/Models/Player.cs
@@ -5,10 +5,10 @@ namespace dotnetapp.Models
 {
     public class Player{
         public int Id{get;set;}
-        [Required(ErrorMessage="Name cannot be blank")]
+        [Required(ErrorMessage="Name is required.")]
         public string Name{get;set;}
         public string Category{get;set;}
-        [Range(1,int.MaxValue,ErrorMessage="Bidding Amount should be greater then 0")]
+        [Range(1,int.MaxValue,ErrorMessage="Bidding amount must be greater than 0.")]
         public decimal BiddingAmount{get;set;}
     }
 }
