92c9c12439b7813d3f0c1ff7fbd12e73f76e26b0
Iamneo <techadmin@iamneo.ai>
2023-11-11 08:32:29 +0000

    Backup Commit

---
+++ dotnetapp/Migrations/20231111082607_InitialSetup2.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetapp.Migrations
+{
+    public partial class InitialSetup2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Team_TeamId",
+                table: "Players");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Team",
+                table: "Team");
+
+            migrationBuilder.RenameTable(
+                name: "Team",
+                newName: "Teams");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Teams",
+                table: "Teams",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "TeamId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Teams",
+                table: "Teams");
+
+            migrationBuilder.RenameTable(
+                name: "Teams",
+                newName: "Team");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Team",
+                table: "Team",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Team_TeamId",
+                table: "Players",
+                column: "TeamId",
+                principalTable: "Team",
+                principalColumn: "TeamId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
